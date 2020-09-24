6e1010f92c1227287c9e7caa08ab72d0d2892959
xuruifeng <xuruifeng@do1.com.cn>
2020-09-24 16:31:43 +0800

    SpringBoot结合Redis做缓存

--- springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
@@ -1,9 +1,13 @@
 package com.example.springbootredis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@MapperScan("com.example.springbootredis.dao")
+@EnableCaching
 public class SpringbootRedisApplication {
 
 	public static void main(String[] args) {
